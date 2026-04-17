a9896b1b9a80b3c6da16d0e0263c50f991b14884
DESKTOP-KM47ILD\Usuário <tjusto2409@gmail.com>
2026-04-17 02:26:18 -0300

    Adjustment import in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { getGeneratedRoutes } from './core/factories/route.factory';
+import { getGeneratedRoutes } from '@core/factories/route.factory';
 
-import '@pages/page';
+import '@pages/index';
 
 export const appConfig: ApplicationConfig = {
   providers: [
